09c67318f6a91944c82233f48d02e6b5f65e2352
Rorical <46294886+Rorical@users.noreply.github.com>
2022-07-22 00:59:30 +0800

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,63 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'KyattsuNetwork',
+  tagline: 'Cats will find the way out.',
+  url: 'https://kyattsu.network',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Kyattsu Network',
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/KyattsuNetwork',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'light',
+        copyright: `Copyright © ${new Date().getFullYear()} Kyattsu Network. Made with ❤️ by Shugetsu Soft.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import clsx from 'clsx';
+import styles from './styles.module.css';
+import { ApiPromise, HttpProvider } from '@polkadot/api';
+
+function Feature({title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+      </div>
+      <div className="text--center padding-horiz--md">
+        <p>{title}</p>
+        <h2>{description}</h2>
+      </div>
+    </div>
+  );
+}
+
+
+class Features extends Component {
+  constructor() {
+    super();
+    this.state = { blockHeight: NaN, timer: 0 };
+  }
+
+  async componentDidMount() {
+    const rpcProvider = new HttpProvider('https://rpc.kyattsu.network');
+    const api = await ApiPromise.create({ provider: rpcProvider });
+    let blockHeight = await (await api.rpc.system.syncState()).currentBlock.toNumber()
+    this.setState({ timer: window.setInterval(() => {
+      this.setState({ blockHeight: this.state.blockHeight + 1 })
+    }, 1000 * 12) })
+    this.setState({ blockHeight: blockHeight })
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.state.timer)
+  }
+
+  render() {
+    return (
+      <section className={styles.features}>
+        <div className="container">
+          <div className="row">
+            <Feature title={"Total Blocks"} description={this.state.blockHeight} />
+            <Feature title={"Token"} description={"NYA"} />
+            <Feature title={"Block Time"} description={"12 Sec"} />
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Features
